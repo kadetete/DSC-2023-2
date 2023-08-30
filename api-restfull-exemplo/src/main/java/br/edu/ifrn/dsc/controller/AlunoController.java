ce55c4f328a78cb9298199b7e6303f926897ee4f
Guilherme Cadete Matias <20221148060001@ifrn.local>
2023-08-30 10:21:06 -0300

    complemento

---
+++ api-restfull-exemplo/src/main/java/br/edu/ifrn/dsc/controller/AlunoController.java
@@ -0,0 +1,11 @@
+package br.edu.ifrn.dsc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("alunos")
+public class AlunoController {
+    
+    public void matricularAluno(){}
+}
---
+++ api-restfull-exemplo/src/main/java/br/edu/ifrn/dsc/domain/aluno/Aluno.java
@@ -0,0 +1,15 @@
+package br.edu.ifrn.dsc.domain.aluno;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Aluno {
+    
+    private String nome;
+    private String matricula;
+
+}
